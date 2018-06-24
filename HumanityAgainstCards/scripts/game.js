f69a774b230416f737949f34cadf753a186aa717
Sam Orme <ormesam@hotmail.com>
2018-06-24 16:03:08 +0100

    render hand as cards

--- HumanityAgainstCards/scripts/game.js
+++ HumanityAgainstCards/scripts/game.js
@@ -8,6 +8,18 @@
         console.log(message);
     }
 
+    function createCard(text) {
+        var card = $("<div>");
+        var cardText = $("<p>");
+
+        card.addClass("card");
+        cardText.text(text);
+
+        card.append(cardText);
+
+        return card;
+    }
+
     gameHub.client.playerJoined = function (name) {
         log("Player joined: " + name);
     };
@@ -22,16 +34,22 @@
         log("New Question: " + question.Value + " (" + question.BlankCount + " blanks)");
 
         currentQuestion = question;
-        $("#question").text(question.Value.replace("_", "_______"));
+
+        var expandedQuestion = question.Value;
+
+        for (var i = 0; i < question.BlankCount; i++) {
+            expandedQuestion = expandedQuestion.replace("_", "_______")
+        }
+
+        $("#question").text(expandedQuestion);
     };
 
     gameHub.client.showHand = function (hand) {
         $("#hand").html("");
 
         for (var i = 0; i < hand.length; i++) {
-            var card = $("<p>");
-            card.text(hand[i].Value);
-            card.addClass("hand-card");
+            var card = createCard(hand[i].Value);
+            card.addClass("card-hand");
             $("#hand").append(card);
         }
     };
@@ -83,10 +101,12 @@
             gameHub.server.start(roomCode);
         });
 
-        $(document).on("click", ".hand-card", function () {
-            gameHub.server.submitCard(roomCode, $(this).text());
-            console.log("Submitted card: " + $(this).text());
+        $(document).on("click", ".card-hand", function () {
+            var cardText = $(this).children("p").first().text();
+            gameHub.server.submitCard(roomCode, cardText);
             $(this).addClass("hidden");
+
+            console.log("Submitted card: " + cardText);
         })
 
         $(document).on("click", ".vote-card", function () {
