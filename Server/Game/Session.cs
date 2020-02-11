419dd2fa7d1db6c758a7ebc979f1cd5b01574098
Sam Orme <ormesam@hotmail.com>
2020-02-11 21:07:33 +0000

    Stub out session start

--- Server/Game/Session.cs
+++ Server/Game/Session.cs
@@ -1,4 +1,5 @@
 ﻿using System.Collections.Generic;
+using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.SignalR;
 using Server.Hubs;
@@ -10,11 +11,24 @@ namespace Server.Game {
         public string Code { get; }
         public GameState GameState { get; private set; }
         public IList<Player> Players { get; set; }
+        public Queue<AnswerCard> AnswerPile { get; set; }
 
         public Session(string code) {
             Code = code;
             GameState = GameState.NotStarted;
             Players = new List<Player>();
+            AnswerPile = new Queue<AnswerCard>();
+        }
+
+        public async Task Start() {
+            while (Players.Any()) {
+                // Deal Cards
+                // Show Question
+                // Wait 30 Seconds with check
+                // Show Answers
+                // Wait for users to pick answers (30 seconds with check)
+                // Display results
+            }
         }
 
         public async Task<GameState> Join(IHubContext<GameHub, IGameClient> gameHub, string connectionId, string name) {
